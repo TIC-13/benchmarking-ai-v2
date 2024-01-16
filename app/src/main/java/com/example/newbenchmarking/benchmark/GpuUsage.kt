f1b917a469a21e82e7b5f9b0ee11d40b1e400e29
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-01-16 19:03:04 -0300

    GPU Usage adreno

--- app/src/main/java/com/example/newbenchmarking/benchmark/GpuUsage.kt
+++ app/src/main/java/com/example/newbenchmarking/benchmark/GpuUsage.kt
@@ -15,14 +15,28 @@ class GpuUsage {
             val percentString = File(filePath).readText().trim()
             usage = percentString.dropLast(1).toInt()
         } catch (e: FileNotFoundException) {
-            println("File not found: $filePath")
-            usage = 0
+            println("Erro na leitura do arquivo em Mali: $filePath")
+            usage = usageAdreno()
         } catch (e: Exception) {
             println("Error reading file: ${e.message}")
             usage = 1
         }
         totalUsage += usage
         numSamples += 1
+    }
+
+    private fun usageAdreno(): Int {
+        val filePath = "/sys/class/kgsl/kgsl-3d0/gpubusy"
+        try {
+            val pair = File(filePath).readText().trim().split(" ")
+            return (pair[0].toInt()/pair[1].toInt())*100
+        }catch(e: FileNotFoundException){
+            println("Erro na leitura do arquivo em Adreno: $filePath")
+            return 0
+        }catch(e: Exception){
+            println("Outro error")
+            return 0
+        }
     }
 
     fun get(): Int {
