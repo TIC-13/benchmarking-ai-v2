55efcd0d9c81c68e5fd763921d21794f09ca5741
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-01-16 12:42:01 -0300

    Uso da interpreter api ao invés da tasklib

--- app/src/main/java/com/example/newbenchmarking/machineLearning/RunTfliteModel.kt
+++ app/src/main/java/com/example/newbenchmarking/machineLearning/RunTfliteModel.kt
@@ -1,21 +1,73 @@
 package com.example.newbenchmarking.machineLearning
 
 import android.content.Context
+import android.content.res.AssetManager
+import android.graphics.Bitmap
 import com.example.newbenchmarking.interfaces.InferenceParams
-import com.example.newbenchmarking.interfaces.ModelType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.gpu.GpuDelegate
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import org.tensorflow.lite.task.core.BaseOptions
+import java.io.FileInputStream
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import kotlin.system.measureTimeMillis
 
-fun runTfliteModel(context: Context, params: InferenceParams, images: List<TensorImage>): Pair<Long, Long> {
-    var builder = BaseOptions.builder().setNumThreads(params.numThreads)
-    if(params.useNNAPI) builder = builder.useNnapi()
-    if(params.useGPU) builder = builder.useGpu()
+fun runTfLiteModel(context: Context, params: InferenceParams, images: List<Bitmap>): Pair<Long, Long> {
 
-    return if(params.model.modelType == ModelType.SEGMENTATION){
-        imageSegmentation(context, params, images, builder)
-    }else if (params.model.modelType == ModelType.CLASSIFICATION){
-        imageClassification(context, params, images, builder)
-    }else{
-        objectDetection(context, params, images, builder)
+    val model = loadModelFile(context.assets, modelName = params.model.filename)
+    val gpuDelegate = GpuDelegate(GpuDelegate.Options().setQuantizedModelsAllowed(true))
+
+    val options = Interpreter.Options().apply {
+        if(params.useGPU){
+            this.addDelegate(gpuDelegate)
+        }
+        useNNAPI = params.useNNAPI
+        numThreads = params.numThreads
     }
+
+    val interpreter: Interpreter
+
+    val loadTime = measureTimeMillis {
+        interpreter = Interpreter(model, options)
+    }
+
+    var totalInferenceTime = 0L
+
+    for(bitmap in images){
+
+        totalInferenceTime += measureTimeMillis {
+
+            val imageProcessorBuilder = ImageProcessor.Builder()
+
+            imageProcessorBuilder
+                .add(ResizeOp(params.model.inputShape[2], params.model.inputShape[1], ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+
+            imageProcessorBuilder.add(CastOp(params.model.inputDataType))
+            val tensorImage = TensorImage(params.model.inputDataType)
+            tensorImage.load(bitmap)
+
+            val input = imageProcessorBuilder.build().process(tensorImage)
+            val output = TensorBuffer.createFixedSize(params.model.outputShape, params.model.outputDataType)
+
+            interpreter.run(input.buffer, output.buffer)
+
+        }
+    }
+
+    val mediumInferenceTime = (totalInferenceTime/images.size)
+    return Pair(loadTime, mediumInferenceTime)
+}
+
+fun loadModelFile(assetManager: AssetManager, modelName: String): MappedByteBuffer {
+    val fileDescriptor = assetManager.openFd(modelName)
+    val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+    val fileChannel = inputStream.channel
+    val startOffset = fileDescriptor.startOffset
+    val declaredLength = fileDescriptor.declaredLength
+    return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
 }
