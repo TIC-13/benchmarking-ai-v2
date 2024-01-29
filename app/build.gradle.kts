377fb67715ac1d6c7845cb00d3902a64b94639b8
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-01-29 09:37:09 -0300

    Status and navigation bar colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- app/src/main/java/com/example/newbenchmarking/MainActivity.kt
+++ app/src/main/java/com/example/newbenchmarking/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -22,9 +23,11 @@ import com.example.newbenchmarking.pages.InferenceConfig
 import com.example.newbenchmarking.pages.ResultScreen
 import com.example.newbenchmarking.pages.RunModel
 import com.example.newbenchmarking.theme.AppTheme
+import com.example.newbenchmarking.theme.LocalAppColors
 import com.example.newbenchmarking.ui.theme.NewBenchmarkingTheme
 import com.example.newbenchmarking.viewModel.InferenceViewModel
 import com.example.newbenchmarking.viewModel.ResultViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +50,14 @@ fun App(modifier: Modifier = Modifier, navController: NavHostController = rememb
 
     val inferenceViewModel = InferenceViewModel()
     val resultViewModel = ResultViewModel()
+
+    val systemController = rememberSystemUiController()
+    systemController.setStatusBarColor(
+        color = LocalAppColors.current.primary
+    )
+    systemController.setNavigationBarColor(
+        color = Color.Black
+    )
 
     NavHost(navController = navController, startDestination = "home") {
         composable(
