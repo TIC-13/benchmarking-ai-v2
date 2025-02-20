e0b1492eb7c4b8cd27bc4955b2e31bcf21689f11
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2025-02-20 12:29:32 -0300

    Bump no código de versão e assets utilizados na commit anterior

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "ai.luxai.speedai"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
